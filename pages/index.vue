a746aef78189c87b96ad47d35741d653b2c1d091
larry-xue <azoux0311@gmail.com>
2024-06-17 21:30:39 +0800

    feat: enable using regexp to exclude dir and depth parameter

--- pages/index.vue
+++ pages/index.vue
@@ -9,15 +9,20 @@ const dirStructure = ref<TreeDir>({
   children: [],
   type: 'directory'
 })
+const form = ref<{
+  excludeDirs: string
+  depth: number
+}>({
+  excludeDirs: '',
+  depth: 0
+})
+const showCopyToast = ref(false)
 
 const getDirHandler = (e: Event) => {
   onprogress.value = true
   window.showDirectoryPicker().then((dir: FileSystemDirectoryHandle) => {
-    traverseDir(dir).then((res) => {
-      console.log('done = ', res)
+    traverseDir(dir, form.value.excludeDirs?.split(','), form.value.depth || -1).then((res) => {
       dirStructure.value = res
-    }).catch((err) => {
-      console.log('err = ', err)
     })
   }).catch((err) => {
     console.log('err = ', err)
@@ -29,16 +34,71 @@ const getDirHandler = (e: Event) => {
 const jsonPreview = computed(() => {
   return JSON.stringify(dirStructure.value, null, 2)
 })
+
+const downloadJson = () => {
+  const blob = new Blob([jsonPreview.value], { type: 'application/json' })
+  const url = window.URL.createObjectURL(blob)
+  const a = document.createElement('a')
+  a.style.display = 'none'
+  a.href = url
+  a.download = 'dirStructure.json'
+  document.body.appendChild(a)
+  a.click()
+  document.body.removeChild(a)
+  window.URL.revokeObjectURL(url)
+}
+
+const copyJson = () => {
+  navigator.clipboard.writeText(jsonPreview.value)
+  showCopyToast.value = true
+  setTimeout(() => {
+    showCopyToast.value = false
+  }, 2000)
+}
 </script>
 
 <template>
-  <div class="w-full mt-12" v-loading="onprogress">
-    <!-- big button on page top -->
-    <button class="btn btn-primary btn-block" @click="getDirHandler">Select Directory</button>
-  </div>
-  <div class="json-preview mt-12">
-    <div class="mockup-code dark:bg-white text-primary-content">
-      <pre><code>{{ jsonPreview }}</code></pre>
+  <div class="w-full">
+    <div class="w-full mt-12" v-loading="onprogress">
+      <div class="flex gap-2">
+        <div>
+          <input class="input input-accent w-full max-w-xs" type="text" placeholder="exclude dirs in regex"
+            v-model="form.excludeDirs">
+          <div class="label">
+            <span class="label-text-alt">
+              Example: node_modules,dist,.vue$
+              <br>
+              <span class="text-slate-400">Use comma to separate, and do not need to include /</span>
+            </span>
+          </div>
+        </div>
+        <div>
+          <input class="input input-accent w-full max-w-xs" type="number" placeholder="depth" v-model="form.depth">
+          <div class="label">
+            <span class="label-text-alt">
+              The depth of the directory
+            </span>
+          </div>
+        </div>
+      </div>
+
+      <!-- big button on page top -->
+      <div class="bg-primary text-white w-full h-96 btn btn-primary mt-10" @click="getDirHandler">
+        <span class="text-white text-9xl">Get Dir</span>
+      </div>
+    </div>
+    <div class="json-preview my-12">
+      <div class="btn-group">
+        <button class="btn btn-link btn-ghost" @click="downloadJson">download json</button>
+        <div class="avatar indicator">
+          <span class="indicator-item badge badge-success" v-if="showCopyToast">copied</span>
+          <button class="btn btn-link btn-ghost" @click="copyJson">copy json</button>
+        </div>
+      </div>
+      <div class="mockup-code max-h-[400px] overflow-auto">
+        <pre v-for="(line, index) in jsonPreview.split('\n')" :key="line"
+          :data-prefix="index + 1"><code>{{ line }}</code></pre>
+      </div>
     </div>
   </div>
 </template>
--- utils/travelDir.ts
+++ utils/travelDir.ts
@@ -4,27 +4,48 @@ export type TreeDir = {
   type: 'file' | 'directory'
 }
 
-export async function traverseDir(dirHandle: FileSystemDirectoryHandle) {
-  const treeDir: TreeDir = {
-    name: dirHandle.name,
-    children: [],
-    type: dirHandle.kind
-  }
-  for await (const [key, value] of dirHandle.entries()) {
-    if (value.kind === 'directory') {
-      const subDir = await traverseDir(value);
-      treeDir.children.push({
-        ...subDir,
-        type: 'directory'
-      });
-    } else {
-      treeDir.children.push({
-        name: key,
-        children: [],
-        type: 'file'
-      })
+/**
+ * Traverses a directory and returns a tree structure of its contents.
+ *
+ * @param {FileSystemDirectoryHandle} dirHandle - The directory handle to traverse.
+ * @param {string[]} [excludeDirs=[]] - An optional array of directory names to exclude from the tree.
+ * @param {number} [depth=-1] - The maximum depth to traverse. Defaults to -1 for unlimited depth.
+ * @return {Promise<TreeDir>} - A promise that resolves to the tree structure of the directory contents.
+ */
+export async function traverseDir(dirHandle: FileSystemDirectoryHandle, excludeDirs: string[] = [], depth = -1): Promise<TreeDir> {
+  const traverse = async (dirHandle: FileSystemDirectoryHandle, excludeDirs: string[], depth: number) => {
+    if (depth === 0) return
+
+    const treeDir: TreeDir = {
+      name: dirHandle.name,
+      children: [],
+      type: dirHandle.kind
     }
+    for await (const [key, value] of dirHandle.entries()) {
+      if (excludeDirs?.length && excludeDirs.some(excludeDir => new RegExp(excludeDir).test(key))) {
+        continue
+      }
+      if (value.kind === 'directory') {
+        const subDir = await traverse(value, excludeDirs, depth - 1);
+        subDir && treeDir.children.push({
+          ...subDir,
+          type: 'directory'
+        });
+      } else {
+        treeDir.children.push({
+          name: key,
+          children: [],
+          type: 'file'
+        })
+      }
+    }
+
+    return treeDir
   }
 
-  return treeDir
+  return traverse(dirHandle, excludeDirs.filter(Boolean), depth) || {
+    name: 'Something went wrong',
+    children: [],
+    type: 'directory'
+  }
 }
